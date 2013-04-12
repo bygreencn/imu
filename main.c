018fa412ea89e8d7fb7851f31d7ed7c412daa0b8
Thomas Gonnot <thomas_vivien_gonnot@msn.com>
2013-04-11 19:58:07 -0500

    Added debug output on USART2 via printf function (also .gitignore)

--- main.c
+++ main.c
@@ -1,11 +1,54 @@
 #define USE_STDPERIPH_DRIVER
 #define STM32F10X_HD
 #include "stm32f10x.h"
+#include <stdio.h>
 
+#define BAUD_RATE 9600
+
+int main(void);
+void hw_init(void);
+int __io_putchar(int ch);
 
 int main()
 {
+	hw_init();
+	while(1);
 	return 0;
+}
+
+void hw_init()
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	USART_InitTypeDef USART_InitStructure;
+
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE); 
+
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	USART_InitStructure.USART_BaudRate = BAUD_RATE;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+
+	USART_Init(USART2, &USART_InitStructure);
+	USART_Cmd(USART2, ENABLE);
+}
+
+int __io_putchar(int ch)
+{
+	while(USART_GetFlagStatus(USART2, USART_FLAG_TC) == RESET);
+	USART_SendData(USART2, (uint8_t) ch);
+	return ch;
 }
 
 #ifdef USE_FULL_ASSERT
